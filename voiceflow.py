03b4e0182ab49bc955502e3762a344c085e7a01a
owenp <panowenzz@gmail.com>
2024-10-22 19:05:35 -0400

    voiceflow augmentation script

--- voiceflow.py
+++ voiceflow.py
@@ -1,9 +1,16 @@
+import json
 import requests
+import os
+import re
+
+from dotenv import load_dotenv
 
 
 class Voiceflow:
-
+    
     def __init__(self, userID):
+        load_dotenv('.env')
+        self.API_KEY = os.getenv('API_KEY')
         self.url = "https://general-runtime.voiceflow.com/state/user/" + userID + "/interact?logs=off"
 
     def launch_workflow(self, variable_map):
@@ -21,7 +28,7 @@ class Voiceflow:
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
-            "Authorization": "VF.DM.66e4e1a6ec948227a6f65180.7f5cMxkTGWfY0Za1"
+            "Authorization": self.API_KEY
         }
 
         return requests.post(self.url, json=payload, headers=headers)
@@ -48,7 +55,7 @@ class Voiceflow:
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
-            "Authorization": "VF.DM.66e4e1a6ec948227a6f65180.7f5cMxkTGWfY0Za1"
+            "Authorization": self.API_KEY
         }
 
         return requests.post(self.url, json=payload, headers=headers)
@@ -70,22 +77,56 @@ class Voiceflow:
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
-            "Authorization": "VF.DM.66e4e1a6ec948227a6f65180.7f5cMxkTGWfY0Za1"
+            "Authorization": self.API_KEY
         }
 
         return requests.post(self.url, json=payload, headers=headers)
 
     def close_flow(self):
-        headers = {"Authorization": "VF.DM.66e4e1a6ec948227a6f65180.7f5cMxkTGWfY0Za1"}
+        headers = {"Authorization": self.API_KEY}
 
         return requests.delete(self.url[:len(self.url) - 18], headers=headers)
 
+class MockText:
+
+    @staticmethod
+    def mock(text):
+        vf = Voiceflow("2")
+        res = vf.launch_workflow({"mock": text}).json()[0]
+        print("=== Res ===")
+        print(res)
+        print("=== End Res ===")
+        children = res["payload"]["slate"]["content"]
+        for child in children:
+            for text in child["children"]:
+                list_queries = json.loads(text["text"])["Queries"]
+                return list_queries
+
+def tokenize(string):
+    return re.findall(r'\w+|[.,!?;]', string)
+    
+
+
 def main():
-    vf = Voiceflow("1")
-    print(vf.launch_workflow( {"myVar": 'a'} ).text)
-    print(vf.send_text( {}, "I'd like to be connected to the head of the sales department for a strategic partnership discussion.").text)
-    # print(vf.send_intent( {}, "I'd like to be connected to the head of the sales department for a strategic partnership discussion.", "" ).text)
-    print(vf.close_flow())
+    # parse data
+    augmented_json = []
+    with open("augmentation_week2/test.json", "r") as input_file:
+        data = json.load(input_file)
+        with open("augmentation_week2/test_augment_2.json", "w") as output_file:
+            try:
+                for query in data:
+                    augmented_json.append(query.copy())
+                    result_list = MockText.mock(query["question"])
+                    for result in result_list:
+                        query["question"] = result
+                        # query["question_toks"] = tokenize(result)
+                        augmented_json.append(query.copy())
+                json.dump(augmented_json, output_file, indent=4)
+            except:
+                json.dump(augmented_json, output_file, indent=4)
+
+
+            
 
 if __name__ == "__main__":
     main()
